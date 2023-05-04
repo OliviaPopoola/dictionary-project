3ece3f00ce066f292335a24ee82e3a12c6e5ef7c
OliviaPopoola <117594203+OliviaPopoola@users.noreply.github.com>
2023-05-04 12:22:43 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      {" "}
+      <div className="container">Hello!</div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Olivia </footer>
+    </div>
+  );
 }
 
 export default App;
